19f5b01b7facea6e2b3bccf8039e04755aa946cc
deepika <deepika.tripathi@appinventiv.com>
2019-10-10 18:05:20 +0530

    update

---
+++ lib/bloc/theme_bloc.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+
+class ThemeBloc 
+{
+  StreamController<bool> _themeController = StreamController<bool>();
+  get changeTheme => _themeController.sink.add;
+  get darkThemeEnabled => _themeController.stream;
+}
